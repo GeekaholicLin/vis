2f951e6b620ca0e4c3a762a533dc54588420b14e
GeekaholicLin <geekaholiclin@gmail.com>
2018-01-20 22:18:32 +0800

    add ultis etc. resolves

--- webpack.config.js
+++ webpack.config.js
@@ -62,9 +62,11 @@ module.exports = {
     alias: {
       src: path.resolve(__dirname, "src"),
       assets: path.resolve(__dirname, "src/assets"),
-      scenes: path.resolve(__dirname, "src/scenes"),
       components: path.resolve(__dirname, "src/components"),
-      myRedux: path.resolve(__dirname, "src/redux")
+      ultis: path.resolve(__dirname, "src/ultis"),
+      HOC: path.resolve(__dirname, "src/HOC"),
+      data: path.resolve(__dirname, "src/data"),
+      constant: path.resolve(__dirname, "src/constant")
     },
     extensions: [".js", ".scss", ".png", ".jpg"],
     modules: ["node_modules", path.resolve(__dirname, "src/assets")]
