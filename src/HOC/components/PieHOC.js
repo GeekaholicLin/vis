0b88b1ddd8c56b8de64e1449a55431ba4f46d418
GeekaholicLin <geekaholiclin@gmail.com>
2018-03-03 20:14:52 +0800

    refactor PieHOC

--- src/HOC/components/PieHOC.js
+++ src/HOC/components/PieHOC.js
@@ -1,21 +1,39 @@
 import React from "react";
 import { Pie } from "components";
 import withSubscriber from "../withSubscriber";
-import { getChartColors, getValuesArrByKeyOrFunc } from "ultis";
-const mapContextToProps = ({
-  data,
-  fill,
-  width,
-  height,
-  dataKey = "value",
-  chartNamespace,
-  __updatedState__
-}) => {
+import {
+  getValuesArrByKeyOrFunc,
+  generatePropsWithDataKey,
+  generateLegendColorsWithDataKey
+} from "ultis";
+const mapContextToProps = (
+  { data, width, height, __legendSelectedItems__ = "all" },
+  { dataKey, nameKey }
+) => {
+  let isAll = __legendSelectedItems__ === "all";
+  let result = [...data];
+  if (!isAll) {
+    result = data.map(d => {
+      if (__legendSelectedItems__.indexOf(d[nameKey]) < 0) {
+        return Object.assign({}, d, { [dataKey]: 0 });
+      }
+      return d;
+    });
+  }
   return {
-    data: getValuesArrByKeyOrFunc(dataKey, __updatedState__.data || data),
+    data: getValuesArrByKeyOrFunc(dataKey, result),
     left: width / 2,
-    top: height / 2,
-    fill: getChartColors(fill, chartNamespace)
+    top: height / 2
   };
 };
-export default withSubscriber({ mapContextToProps })(Pie);
+const hoistPropsToContext = ({ nameKey, fill }) => {
+  return Object.assign({}, generateLegendColorsWithDataKey("__pie__", fill), {
+    nameKey
+  });
+};
+const skipPropsKeys = ["dataKey", "nameKey"];
+export default withSubscriber({
+  mapContextToProps,
+  hoistPropsToContext,
+  skipPropsKeys
+})(Pie);
